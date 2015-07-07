a485691d7ceb885f488edd8d7c1835c1349697bb
Noruega <suecia.noruega@gmail.com>
2015-07-07 23:15:43 +0200

    Re-add Heroku

--- config/config.js
+++ config/config.js
@@ -8,6 +8,10 @@ exports.port = 8000;
 //   know what you are doing.
 exports.proxyip = false;
 
+-// Set this to true if you are using Pokemon Showdown on Heroku.
+-exports.herokuhack = true;
+-
+
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
 //   in every Random Battle team.
